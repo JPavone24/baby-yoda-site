05d355a0d0645073e6d490d0fa6e8052bd5bec5b
Jessica Pavone <JessicaPavone24@yahoo.com>
2019-12-02 21:14:47 -0800

    added memes and videos

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express")
 var app = express()
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
-    res.sendFile(_dirname + '/index.html')
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/memes', function(req, res){
+    res.sendFile(__dirname + '/public/memes.html')
+})
+
+app.get('/videos', function(req, res){
+    res.sendFile(__dirname + '/public/videos.html')
 })
 
 app.listen(process.env.port || 4000, function(){
