c4a1a02392c4302b9672ea18870c1e599b5c52d1
Jessica Pavone <JessicaPavone24@yahoo.com>
2019-12-09 21:43:20 -0800

    sending

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 var express = require("express")
 var app = express()
+var path = require("path")
+const mongoose = require('mongoose');
+var fs = require("fs")
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/merch"));
+app.use(express.static(__dirname + "/public/pages"));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html')
+
+// app.set('view engine', 'ejs')
+//mongo
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true ,  useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
 
-app.get('/memes', function(req, res){
-    res.sendFile(__dirname + '/public/memes.html')
-})
+//routes
 
-app.get('/videos', function(req, res){
-    res.sendFile(__dirname + '/public/videos.html')
-})
+var htmlroutes = require('./routes/htmlroutes');
+var apiroutes = require('./routes/postroutes');
+var merchroutes = require('./routes/merchroutes');
 
-app.listen(process.env.port || 4000, function(){
+app.use('/', htmlroutes);
+app.use('/api', apiroutes);
+app.use('/merch', merchroutes);
+
+ app.listen(process.env.port || 8080, function(){
     console.log("server running")
 })
+
+
+
